ae0147016ead06aa4f283bfcd5f9830b75f5254f
Azryl Shamin <azryl.career@gmail.com>
2025-04-16 19:41:57 +0800

    Enable user to play a round of the game

--- javascript.js
+++ javascript.js
@@ -1,4 +1,13 @@
+let humanScore = 0;
+let computerScore = 0;
 
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
+
+console.log(humanSelection);
+console.log(computerSelection);
 
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3)
@@ -14,7 +23,61 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-    return prompt("Enter your choice: ")
+    let input = prompt("Enter your choice: ").toLowerCase();
+    return input;
 }
 
-let choice = getHumanChoice();
+function playRound(humanChoice, computerChoice) {
+    let roundMessage;
+
+    if (humanChoice === "rock") {
+        switch (computerChoice) {
+            case "rock":
+                roundMessage = "It's a tie! Both of you choose Rock";
+                break;
+            case "paper":
+                roundMessage = "You lose! Paper beats Rock";
+                computerScore++;
+                break;
+            case "scissors":
+                roundMessage = "You win! Rock beats Scissors";
+                humanScore++;
+                break;
+        }
+    }
+
+    if (humanChoice == "paper") {
+        switch (computerChoice) {
+            case "rock":
+                roundMessage = "You win! Paper beats Rock";
+                humanScore++;
+                break;
+            case "paper":
+                roundMessage = "It's a tie! Both of you choose Paper";
+                break;
+            case "scissors":
+                roundMessage = "You lose! Scissors beats Paper";
+                computerScore++;
+                break;
+        }
+    }
+
+    if (humanChoice == "scissors") {
+        switch (computerChoice) {
+            case "rock":
+                roundMessage = "You lose! Rock beats Scissors";
+                computerScore++;
+                break;
+            case "paper":
+                roundMessage = "You win! Scissors beats Paper";
+                humanScore++;
+                break;
+            case "scissors":
+                roundMessage = "It's a tie! Both of you choose Scissors";
+                break;
+        }
+    }
+
+    console.log(roundMessage);
+}
+
